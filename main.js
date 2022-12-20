0386e4dcdb64c5b54cd3fd8d821e574b24cabbb4
Kainoa Ubaldo-Brabo <kaibrabo@gmail.com>
2022-12-20 15:35:08 -0800

    mvp complete

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const editor = document.getElementById('editor');
 const preview = document.getElementById('preview');
 
+// initial launch
+updatePreview();
+
 editor.addEventListener('keyup', updatePreview);
 
 function updatePreview() {
-    preview.innerHTML = editor.value;
+    preview.innerHTML = marked.parse(editor.value);
 }
